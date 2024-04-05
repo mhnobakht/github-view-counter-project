049e01d6ad89af6246cdcfd61e2e65afc8b47a5d
mhnobakht <01programmingacademy@gmail.com>
2024-04-06 02:49:33 +0330

    make dbs connection

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
 
-use Classes\Users\Messages\Message;
+use Classes\Database;
 
 require_once 'Autoloader.php';
 
-Message::hello();
+$dbs = new Database();
+
+$data = [
+    'name' => 'ali',
+    'email' => 'ali@gmail.com',
+    'password' => md5('ali!@#123456789')
+];
+
+$result = $dbs->insert('users', $data);
+
+var_dump($result);
