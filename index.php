12c2880aa2288ce2ba9446b2e20c95a021f95045
mhnobakht <01programmingacademy@gmail.com>
2024-04-07 20:59:35 +0330

    register user

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
 <?php
 
-use Classes\Database;
-
 require_once 'Autoloader.php';
 
-$dbs = new Database();
-
-$data = [
-    'name' => 'ali',
-    'email' => 'ali@gmail.com',
-    'password' => md5('ali!@#123456789')
-];
-
-$result = $dbs->insert('users', $data);
-
-var_dump($result);
